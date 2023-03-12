2d8f839d1d3ec654c1ea598778060f35e1b93b70
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2023-03-12 16:04:58 +0530

    added code for the direct methods

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+namespace AzCourse.IoT.Method
+{
+    public class Program
+    {
+        const string DEVICE_CONNECTION_STRING="";
+       public static async Task<int> Main(string[] args)
+        {
+            using var deviceClient = DeviceClient.CreateFromConnectionString(
+                DEVICE_CONNECTION_STRING,
+                TransportType.Amqp);
+            var sample = new MethodSample(deviceClient);
+            await sample.RunMethod();
+            await deviceClient.CloseAsync();
+
+            Console.WriteLine("Done.");
+            return 0;
+        }
+    }
+}
